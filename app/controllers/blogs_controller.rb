4cde04dc70eae8c33eda79939941ac64573b384a
tsharrad <sharradaudio@gmail.com>
2019-11-27 12:40:49 +1030

    Blog toggle status implemented

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -57,14 +57,25 @@ class BlogsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.friendly.find(params[:id])
-    end
+  def toggle_status
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
-end
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!   
+    end 
+    
+    redirect_to blogs_url, notice: 'Post status has been updated.'
+  end
+
+  private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_blog
+        @blog = Blog.friendly.find(params[:id])
+      end
+
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def blog_params
+        params.require(:blog).permit(:title, :body)
+      end
+  end
