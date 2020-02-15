d051b4d4461e1cd7d588d27aaca82daf3330f75e
tsharrad <sharradaudio@gmail.com>
2020-02-15 13:04:47 +1030

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+  def first_name
+  	self.name.split.first
+  	# the first is taking the first item in the array
+  end
+
+  def last_name
+  	self.name.split.last
+  	# the last is taking the last item in the array
+  end
+
 end
